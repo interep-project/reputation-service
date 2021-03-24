64273d85d58f860aa8a80d13d2b898b78a405a91
Raphael Roullet <raroullet@gmail.com>
2021-03-24 18:35:16 +0100

    add tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import "src/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
