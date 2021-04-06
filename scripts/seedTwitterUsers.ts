5074ed58221709f853cfa7c7cc52a2a7c7efa3d3
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-04-06 15:42:47 +0200

    improve script to seed database with list of seed twitter users

---
+++ scripts/seedTwitterUsers.ts
@@ -0,0 +1,24 @@
+export default [
+  // "twobitidiot",
+  // "La__Cuen", 10k+ friends
+  // "VitalikButerin",
+  // "FEhrsam",
+  "byrongibson",
+  "arcalinea",
+  // "kumavis_",
+  // "whyrusleeping",
+  // "juanbenet",
+  // "karl_dot_tech",
+  // "jinglanW",
+  // "jillruthcarlson",
+  // "zmanian", 10k+ friends
+  // "notscottmoore",
+  // "RaphaelRoullet",
+  // "nicksdjohnson",
+  // "emilianobonassi",
+  // "kaiynne",
+  // "RuneKek",
+  // "haydenzadams",
+  // "sassal0x",
+  // "drakefjustin",
+];
