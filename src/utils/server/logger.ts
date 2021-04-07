96a3849ea1bc397cdb463e68110610bf5bf71313
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-04-07 11:17:22 +0200

    add winston logger

---
+++ src/utils/server/logger.ts
@@ -0,0 +1,47 @@
+import winston from "winston";
+
+let logger: winston.Logger | undefined = undefined;
+
+const colors = {
+  error: "red",
+  warn: "yellow",
+  info: "green",
+  http: "magenta",
+  debug: "white",
+};
+
+const setLogger = (): winston.Logger => {
+  if (logger) {
+    return logger;
+  }
+
+  winston.addColors(colors);
+
+  const format = winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
+    winston.format.colorize({ all: true }),
+    winston.format.printf(
+      (info) => `${info.timestamp} ${info.level}: ${info.message}`
+    )
+  );
+
+  const transports: winston.transport[] = [new winston.transports.Console()];
+  if (process.env.NODE_ENV === "production") {
+    transports.push(
+      new winston.transports.File({
+        filename: "logs/error.log",
+        level: "error",
+      }),
+      new winston.transports.File({ filename: "logs/all.log" })
+    );
+  }
+
+  logger = winston.createLogger({
+    format,
+    transports,
+  });
+
+  return logger;
+};
+
+export default logger || setLogger();
