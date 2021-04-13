8cee0e030f4759d2dec280e98dede27e963a2395
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-04-13 10:38:45 +0200

    add next auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from "next/app";
+import { Provider } from "next-auth/client";
 import "src/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
