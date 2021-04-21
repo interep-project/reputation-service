029b035d570d94f1502f154432ae99eece59f5f6
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-04-21 11:07:34 +0200

    add account linking section

--- src/components/ActionSection/ActionSection.tsx
+++ src/components/ActionSection/ActionSection.tsx
@@ -31,7 +31,7 @@ const ActionSection: FC<ActionSectionProps> = ({
             onClick={onClick}
             type="button"
             className={
-              "inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm" +
+              "inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm" +
               buttonClassname
             }
           >
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,19 +60,20 @@ export default function Home() {
             title="Web 3 Authentication"
             onClick={() => connect && connect()}
             buttonText={connected ? "CHANGE WALLET" : "CONNECT WALLET"}
-            buttonClassname=" bg-indigo-600 hover:bg-indigo-700"
+            buttonClassname=" bg-blue-600 hover:bg-blue-700"
             text={
               connected
                 ? `You are connected with ${address}`
                 : "Connect your wallet to get started"
             }
           />
+          {/* Twitter section */}
           <div className="px-4 pb-5 sm:p-6 flex flex-col">
             <h2 className="text-xl mb-3 leading-6 font-medium text-gray-900">
               Web 2 Authentication
             </h2>
             <h3 className="text-base text-blue-500">Twitter</h3>
-            <div className="mt-2 sm:flex sm:items-center sm:justify-between">
+            <div className="sm:flex sm:items-center sm:justify-between">
               <div className="max-w-xl text-base text-gray-700">
                 <p>
                   {session
@@ -84,10 +85,10 @@ export default function Home() {
                 <button
                   onClick={() => (hasASession ? signOut() : signIn())}
                   type="button"
-                  className={`inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm ${
+                  className={`inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm ${
                     hasASession
                       ? " bg-red-700 hover:bg-red-800"
-                      : " bg-indigo-600 hover:bg-indigo-700"
+                      : " bg-blue-600 hover:bg-blue-700"
                   }`}
                 >
                   {hasASession ? "LOG OUT" : "SIGN IN"}
@@ -99,6 +100,25 @@ export default function Home() {
                 Twitter reputation: {twitterUser.reputation}
               </p>
             ) : null}
+          </div>
+          <div className="px-4 pb-5 sm:p-6 flex flex-col">
+            <h2 className="text-xl mb-3 leading-6 font-medium text-gray-900">
+              Account Linking
+            </h2>
+            <div className="sm:flex sm:items-center sm:justify-between">
+              <div className="max-w-xl text-base text-gray-700">
+                <p>Your account is not linked to any Ethereum address</p>
+              </div>
+              <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
+                <button
+                  onClick={() => null}
+                  type="button"
+                  className={`inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm  bg-blue-600 hover:bg-blue-700`}
+                >
+                  LINK ADDRESS
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
