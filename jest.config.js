89423c87a676c315ce5c32d6d44c2c83f5d813eb
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-04-29 11:53:32 +0100

    add coverage info

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,16 @@ module.exports = {
   testEnvironment: "node",
   moduleDirectories: ["node_modules", "src"],
   modulePaths: ["<rootDir>"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/mocks/**",
+    "!src/models/**",
+    "!src/types/**",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+  ],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
 };
