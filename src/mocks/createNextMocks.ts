bee0f008b2c0e03f7b542d1c84ebfb2a17ab52a4
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-04-30 18:09:18 +0100

    cleaning

--- src/mocks/createNextMocks.ts
+++ src/mocks/createNextMocks.ts
@@ -6,12 +6,10 @@ import {
 } from "node-mocks-http";
 import { NextApiRequest, NextApiResponse } from "next";
 
-// TODO: Move into an util
 const createNextMocks = (
   reqOptions?: RequestOptions
 ): Mocks<NextApiRequest, NextApiResponse> =>
   createMocks<NextApiRequest, NextApiResponse>({
-    // @ts-ignore: GET is part of expected type - RequestMethod - so not sure what is going on
     method: "GET" as RequestMethod,
     ...reqOptions,
   });
