2c045e97c216590e5a5a321f0223a2262b320175
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-01 14:23:20 +0100

    add bundle analyzer and cache dir

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   publicRuntimeConfig: {
     // Will be available on both server and client
     networkId: 42,
   },
-};
+});
