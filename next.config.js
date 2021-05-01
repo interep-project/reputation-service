4ab452a78a19cd34d4a440ac72954b5e2da9d260
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-01 15:09:45 +0100

    remove bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer({
+module.exports = {
   publicRuntimeConfig: {
     // Will be available on both server and client
     networkId: 42,
   },
-});
+};
