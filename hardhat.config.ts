e0874d77ca7394bc9d348a5a2d63876e64ee0680
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-10 13:47:17 +0100

    add solidity coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,12 @@
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (_args, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(await account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+import "solidity-coverage";
 
 const config: HardhatUserConfig = {
-  solidity: "0.7.3",
+  solidity: {
+    version: "0.7.3",
+  },
   paths: {
     sources: "src/contracts",
     tests: "src/tests/contracts",
