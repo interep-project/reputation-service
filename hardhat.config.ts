45683d0fe16c752b0ae6367a879f27d83f48405f
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-10 11:20:47 +0100

    hardhat init

---
+++ hardhat.config.ts
@@ -0,0 +1,21 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (_args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: "0.7.3",
+};
+
+export default config;
