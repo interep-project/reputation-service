c6b23d37162f10016c6477fd803a5185f5cd3988
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-11 15:25:11 +0100

    turn TwitterBadge into ReputationBadge

--- src/contracts/ReputationBadge.sol
+++ src/contracts/ReputationBadge.sol
@@ -4,11 +4,12 @@ pragma solidity ^0.8.0;
 import "./Badge.sol";
 import "./Controlled.sol";
 
-contract TwitterBadge is Badge, Controlled {
-    constructor(address backendAddress_)
-        Badge("TwitterBadge", "TWITT")
-        Controlled(backendAddress_)
-    {}
+contract ReputationBadge is Badge, Controlled {
+    constructor(
+        string memory badgeName_,
+        string memory badgeSymbol_,
+        address backendAddress_
+    ) Badge(badgeName_, badgeSymbol_) Controlled(backendAddress_) {}
 
     function mint(address to, uint256 tokenId) external {
         require(msg.sender == _backendAddress, "Unauthorized");
--- src/tests/contracts/ReputationBadge.ts
+++ src/tests/contracts/ReputationBadge.ts
@@ -1,46 +1,53 @@
 import hre from "hardhat";
 import { expect } from "chai";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
-import { TwitterBadge } from "typechain";
+import { ReputationBadge } from "typechain";
 
 const { ethers } = hre;
 
-describe("TwitterBadge", () => {
+describe("ReputationBadge", () => {
   let owner: SignerWithAddress;
   let backend: SignerWithAddress;
   let signer2: SignerWithAddress;
   let signer3: SignerWithAddress;
-  let twitterBadge: TwitterBadge;
+  let reputationBadge: ReputationBadge;
+
+  const badgeName = "TwitterBadge";
+  const badgeSymbol = "TWITT";
 
   before(async function () {
     [owner, backend, signer2, signer3] = await hre.ethers.getSigners();
   });
 
   beforeEach(async function () {
-    const TwitterBadgeFactory = await ethers.getContractFactory("TwitterBadge");
-    twitterBadge = await TwitterBadgeFactory.connect(owner).deploy(
+    const ReputationBadgeFactory = await ethers.getContractFactory(
+      "ReputationBadge"
+    );
+    reputationBadge = await ReputationBadgeFactory.connect(owner).deploy(
+      badgeName,
+      badgeSymbol,
       backend.address
     );
   });
 
   describe("constructor", () => {
     it("should have the right name", async () => {
-      expect(await twitterBadge.name()).to.eq("TwitterBadge");
+      expect(await reputationBadge.name()).to.eq(badgeName);
     });
 
     it("should have the right symbol", async () => {
-      expect(await twitterBadge.symbol()).to.eq("TWITT");
+      expect(await reputationBadge.symbol()).to.eq(badgeSymbol);
     });
 
     it("should have the right owner", async () => {
-      expect(await twitterBadge.owner()).to.eq(owner.address);
+      expect(await reputationBadge.owner()).to.eq(owner.address);
     });
   });
 
   describe("minting", () => {
     it("should restrict minting to the backend address", async () => {
       await expect(
-        twitterBadge.connect(signer2).mint(signer2.address, 1)
+        reputationBadge.connect(signer2).mint(signer2.address, 1)
       ).to.be.revertedWith("Unauthorized");
     });
 
@@ -48,12 +55,12 @@ describe("TwitterBadge", () => {
       const tokenRecipient = signer2.address;
       const tokenId = 42;
 
-      const mintTx = await twitterBadge
+      const mintTx = await reputationBadge
         .connect(backend)
         .mint(tokenRecipient, tokenId);
       await mintTx.wait();
 
-      expect(await twitterBadge.tokenOf(tokenRecipient)).to.eq(tokenId);
+      expect(await reputationBadge.tokenOf(tokenRecipient)).to.eq(tokenId);
     });
   });
 
@@ -64,32 +71,32 @@ describe("TwitterBadge", () => {
       tokenOwner = signer3;
 
       // mint token with `tokenId` to `tokenOwner` before each test
-      const mintTx = await twitterBadge
+      const mintTx = await reputationBadge
         .connect(backend)
         .mint(tokenOwner.address, tokenId);
       await mintTx.wait();
 
-      expect(await twitterBadge.tokenOf(tokenOwner.address)).to.eq(tokenId);
+      expect(await reputationBadge.tokenOf(tokenOwner.address)).to.eq(tokenId);
     });
 
     it("should not let anyone burn", async () => {
       await expect(
-        twitterBadge.connect(owner).burn(tokenId)
+        reputationBadge.connect(owner).burn(tokenId)
       ).to.be.revertedWith("Unauthorized");
     });
 
     it("should let the backend burn a token", async () => {
-      const burnTx = await twitterBadge.connect(backend).burn(tokenId);
+      const burnTx = await reputationBadge.connect(backend).burn(tokenId);
       await burnTx.wait();
 
-      expect(await twitterBadge.tokenOf(tokenOwner.address)).to.eq(0);
+      expect(await reputationBadge.tokenOf(tokenOwner.address)).to.eq(0);
     });
 
     it("should let owners burn their token", async () => {
-      const burnTx = await twitterBadge.connect(tokenOwner).burn(tokenId);
+      const burnTx = await reputationBadge.connect(tokenOwner).burn(tokenId);
       await burnTx.wait();
 
-      expect(await twitterBadge.tokenOf(tokenOwner.address)).to.eq(0);
+      expect(await reputationBadge.tokenOf(tokenOwner.address)).to.eq(0);
     });
   });
 });
