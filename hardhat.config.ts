b5df06ba177b2efe771bac7e572ddb4d8ab0d40b
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-14 09:53:12 +0100

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,39 @@
+import { resolve } from "path";
+import { config as dotenvConfig } from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "solidity-coverage";
 import "hardhat-gas-reporter";
 
+import "tsconfig-paths/register";
+
+dotenvConfig({ path: resolve(__dirname, "./.env") });
+
+const networkByEnv = {
+  test: "hardhat",
+  development: "localhost",
+  production: "kovan",
+};
+
+const getDefaultNetwork = () => {
+  const env = process.env.NODE_ENV;
+  return env in networkByEnv ? networkByEnv[env] : "hardhat";
+};
+
 const config: HardhatUserConfig = {
+  defaultNetwork: getDefaultNetwork(),
   solidity: {
     version: "0.8.0",
+  },
+  networks: {
+    kovan: {
+      url: process.env.INFURA_KOVAN_RPC_URL,
+      accounts: [
+        `0x${process.env.DEPLOYER_PRIVATE_KEY}`,
+        `0x${process.env.BACKEND_PRIVATE_KEY}`,
+      ],
+    },
   },
   paths: {
     sources: "src/contracts",
