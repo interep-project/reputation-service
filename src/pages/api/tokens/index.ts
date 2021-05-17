aa9a93922f71abd0cfa3a889d8a71a859e55f12b
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-17 14:49:10 +0100

    move tokens by address logic

--- src/pages/api/tokens/index.ts
+++ src/pages/api/tokens/index.ts
@@ -1,10 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import checkAndUpdateTokenStatus from "src/core/blockchain/ReputationBadge/checkAndUpdateTokenStatus";
-import Token from "src/models/tokens/Token.model";
+import TokenController from "src/controllers/TokenController";
 import { ITokenDocument } from "src/models/tokens/Token.types";
-import { getChecksummedAddress } from "src/utils/crypto/address";
 import { dbConnect } from "src/utils/server/database";
-import logger from "src/utils/server/logger";
 
 export default async (
   req: NextApiRequest,
@@ -14,25 +11,7 @@ export default async (
 
   if (req.method !== "GET") {
     return res.status(405).end();
-  }
-  try {
-    const owner = req.query.owner;
-    if (!owner || typeof owner !== "string") {
-      return res.status(400).end();
-    }
-    const ownerChecksummedAddress = getChecksummedAddress(owner);
-
-    if (!ownerChecksummedAddress) return res.status(200).send({ tokens: [] });
-
-    const tokens = await Token.findByUserAddress(ownerChecksummedAddress);
-
-    if (!tokens) return res.status(200).send({ tokens: [] });
-
-    await checkAndUpdateTokenStatus(tokens);
-
-    return res.status(200).send({ tokens });
-  } catch (err) {
-    logger.error(err);
-    return res.status(500).end();
+  } else {
+    return TokenController.getTokensByAddress(req, res);
   }
 };
