bc03b3801f11c1149aeeb62b448a7422d345c8e6
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-17 16:14:52 +0100

    add api endpoint to unlink

---
+++ src/pages/api/linking/unlink.ts
@@ -0,0 +1,61 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import jwt from "next-auth/jwt";
+import config from "src/config";
+import Token from "src/models/tokens/Token.model";
+import { TokenStatus } from "src/models/tokens/Token.types";
+import Web2Account from "src/models/web2Accounts/Web2Account.model";
+import { JWToken } from "src/types/nextAuth/token";
+import { dbConnect } from "src/utils/server/database";
+import logger from "src/utils/server/logger";
+
+// TODO: update after token schema change
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  await dbConnect();
+
+  try {
+    const address = req.query.address;
+    if (!address || typeof address !== "string") {
+      return res.status(400).end();
+    }
+    // @ts-ignore: secret exists
+    const token: JWToken = await jwt.getToken({
+      req,
+      secret: config.JWT_SECRET,
+    });
+
+    // get web2acountId
+    const web2AccountId = token.web2AccountId;
+
+    if (!web2AccountId) {
+      return res.status(403).end();
+    }
+    // get all tokens associated with owner's address
+    const tokens = await Token.findByUserAddress(address);
+
+    // Filter by web2Account id
+    const tokensAssociatedWithWeb2account = tokens?.filter(
+      (token) => token.web2Account === web2AccountId
+    );
+
+    const areAllTokensBurned = tokensAssociatedWithWeb2account?.every(
+      (token) => token.status === TokenStatus.BURNED
+    );
+
+    const web2Account = await Web2Account.findById(web2AccountId);
+
+    if (areAllTokensBurned && web2Account) {
+      web2Account.isLinkedToAddress = false;
+      await web2Account.save();
+
+      return res.status(200).send("Accounts were successfully un-linked");
+    }
+
+    return res.status(500).end();
+  } catch (err) {
+    logger.error(err);
+    return res.status(500).end();
+  }
+};
