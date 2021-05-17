01a447d1310ef0604e5a4e120116233b66918a7e
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-17 12:25:31 +0100

    add transaction schema, update token schema

--- src/models/tokens/Token.schema.ts
+++ src/models/tokens/Token.schema.ts
@@ -1,12 +1,26 @@
 import { Schema } from "mongoose";
+import { TransactionSchema } from "../transactions/Transaction.schema";
+import { Web2Providers } from "../web2Accounts/Web2Account.types";
 import { findByUserAddress } from "./Token.statics";
-import { IToken, ITokenDocument, ITokenModel } from "./Token.types";
+import {
+  IToken,
+  ITokenDocument,
+  ITokenModel,
+  TokenStatus,
+} from "./Token.types";
 
 const TokenSchemaFields: Record<keyof IToken, any> = {
   userAddress: { type: String, index: true },
   issuanceTimestamp: { type: Date, required: true },
   web2Account: { type: Schema.Types.ObjectId, index: true, unique: true },
-  idHash: { type: String, index: true },
+  idHash: { type: String, index: true, required: true },
+  status: { type: String, enum: Object.values(TokenStatus), required: true },
+  mintTransactions: [TransactionSchema],
+  web2Provider: {
+    type: String,
+    enum: Object.values(Web2Providers),
+    required: true,
+  },
 };
 
 const UserSchema = new Schema<ITokenDocument, ITokenModel>(TokenSchemaFields);
---
+++ src/models/transactions/Transaction.types.ts
@@ -0,0 +1,12 @@
+export interface ITransaction {
+  response: {
+    hash: string;
+    blockNumber?: number;
+    timestamp?: number;
+    chainId: number;
+  };
+  receipt?: {
+    status: 0 | 1;
+    confirmations: number;
+  };
+}
