48af07ce9759e62473105e470e757ace89eb8755
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-18 15:06:01 +0100

    update test

---
+++ src/mocks/createMockToken.ts
@@ -0,0 +1,16 @@
+import { ITokenDocument, TokenStatus } from "src/models/tokens/Token.types";
+import { Web2Providers } from "src/models/web2Accounts/Web2Account.types";
+import { getDefaultNetworkId } from "src/utils/crypto/getDefaultNetwork";
+
+const createMockTokenObject = (override?: Partial<ITokenDocument>) => ({
+  contractAddress: "0x8398bCD4f633C72939F9043dB78c574A91C99c0A",
+  web2Account: "608c4a10c994a377e232df7f",
+  issuanceTimestamp: Date.now(),
+  web2Provider: Web2Providers.TWITTER,
+  chainId: getDefaultNetworkId(),
+  status: TokenStatus.NOT_MINTED,
+  idHash: "0x03dd40b36474bf4559c4d733be6f5ec1e61bcb562d1c7f04629ee3af7ee569f9",
+  ...override,
+});
+
+export default createMockTokenObject;
