f58537b75d9c2308cdc55d916f9b8e9d8d394d9e
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-20 15:16:01 +0100

    better handle network change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,7 +60,9 @@ export default function Home() {
     undefined
   );
   const [accountLinkingMessage, setAccountLinkingMessage] = useState("");
-  const [isOnProperNetwork, setIsOnProperNetwork] = useState(false);
+  const [isOnProperNetwork, setIsOnProperNetwork] = useState<boolean | null>(
+    null
+  );
 
   const { tokens, refetchTokens } = useMyTokens(address);
 
@@ -73,9 +75,14 @@ export default function Home() {
         )} network`
       );
     } else if (networkId && networkId === publicRuntimeConfig.networkId) {
-      setIsOnProperNetwork(true);
+      // user switched from wrong to right network, force-reload the page
+      if (isOnProperNetwork === false) {
+        window.location.reload();
+      } else if (isOnProperNetwork === null) {
+        setIsOnProperNetwork(true);
+      }
     }
-  }, [networkId]);
+  }, [networkId, setIsOnProperNetwork]);
 
   const currentNetworkName = useMemo(
     () =>
@@ -183,7 +190,7 @@ export default function Home() {
         refetchTokens();
       }
     },
-    [refetchTokens, signer]
+    [refetchTokens, signer, networkId]
   );
 
   return (
@@ -195,123 +202,125 @@ export default function Home() {
         onAddressClick={() => connect && connect()}
       />
       <div className="max-w-7xl mx-auto mt-10 sm:px-6 lg:px-8">
-        <div className="max-w-2xl mx-auto bg-white shadow sm:rounded-lg">
-          <ActionSection
-            title="Web 3 Authentication"
-            onClick={() => connect && connect()}
-            buttonText={connected ? "CHANGE WALLET" : "CONNECT WALLET"}
-            buttonClassname=" bg-blue-600 hover:bg-blue-700"
-            text={
-              connected
-                ? `You are connected with ${address}`
-                : "Connect your wallet to get started"
-            }
-          />
-          {/* Twitter section */}
-          <div className="px-4 pb-5 sm:p-6 flex flex-col">
-            <h2 className="text-xl mb-3 leading-6 font-medium text-gray-900">
-              Web 2 Authentication
-            </h2>
-            <h3 className="text-base text-blue-500">Twitter</h3>
-            <div className="sm:flex sm:items-center sm:justify-between">
-              <div className="max-w-xl text-base text-gray-700">
-                <p>
-                  {session
-                    ? `You are connected as ${session?.user?.name}`
-                    : "Sign in with Twitter"}
-                </p>
+        {isOnProperNetwork && (
+          <div className="max-w-2xl mx-auto bg-white shadow sm:rounded-lg">
+            <ActionSection
+              title="Web 3 Authentication"
+              onClick={() => connect && connect()}
+              buttonText={connected ? "CHANGE WALLET" : "CONNECT WALLET"}
+              buttonClassname=" bg-blue-600 hover:bg-blue-700"
+              text={
+                connected
+                  ? `You are connected with ${address}`
+                  : "Connect your wallet to get started"
+              }
+            />
+            {/* Twitter section */}
+            <div className="px-4 pb-5 sm:p-6 flex flex-col">
+              <h2 className="text-xl mb-3 leading-6 font-medium text-gray-900">
+                Web 2 Authentication
+              </h2>
+              <h3 className="text-base text-blue-500">Twitter</h3>
+              <div className="sm:flex sm:items-center sm:justify-between">
+                <div className="max-w-xl text-base text-gray-700">
+                  <p>
+                    {session
+                      ? `You are connected as ${session?.user?.name}`
+                      : "Sign in with Twitter"}
+                  </p>
+                </div>
+                <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
+                  <button
+                    onClick={() => (hasASession ? signOut() : signIn())}
+                    type="button"
+                    className={`inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm ${
+                      hasASession
+                        ? " bg-red-700 hover:bg-red-800"
+                        : " bg-blue-600 hover:bg-blue-700"
+                    }`}
+                  >
+                    {hasASession ? "LOG OUT" : "SIGN IN"}
+                  </button>
+                </div>
               </div>
-              <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
-                <button
-                  onClick={() => (hasASession ? signOut() : signIn())}
-                  type="button"
-                  className={`inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm ${
-                    hasASession
-                      ? " bg-red-700 hover:bg-red-800"
-                      : " bg-blue-600 hover:bg-blue-700"
-                  }`}
-                >
-                  {hasASession ? "LOG OUT" : "SIGN IN"}
-                </button>
+              {twitterReputation?.basicReputation ? (
+                <p className="text-base text-gray-700">
+                  Twitter reputation: {twitterReputation.basicReputation}
+                </p>
+              ) : null}
+            </div>
+            <div className="px-4 pb-5 sm:p-6 flex flex-col">
+              <h2 className="text-xl mb-3 leading-6 font-medium text-gray-900">
+                Account Linking
+              </h2>
+              <div className="sm:flex sm:items-center sm:justify-between">
+                <div className="max-w-xl text-base text-gray-700">
+                  <p>{!connected && "Please connect your wallet first."}</p>
+                  <p>{!hasASession && "Please sign in to a web 2 account."}</p>
+                  <p>
+                    {hasASession &&
+                      isCurrentAccountLinked &&
+                      "Your account is already linked to an Ethereum address."}
+                  </p>
+                  <p>
+                    {hasASession &&
+                      !isCurrentAccountLinked &&
+                      "Your account is not linked to any Ethereum address."}
+                  </p>
+                  <p>{accountLinkingMessage}</p>
+                </div>
+                <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
+                  <button
+                    disabled={
+                      !connected ||
+                      !hasASession ||
+                      isCurrentAccountLinked ||
+                      !isOnProperNetwork
+                    }
+                    onClick={() => signAssociation()}
+                    type="button"
+                    className={`inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm  bg-blue-600 hover:bg-blue-700 disabled:bg-gray-300`}
+                  >
+                    LINK ADDRESS
+                  </button>
+                </div>
               </div>
             </div>
-            {twitterReputation?.basicReputation ? (
-              <p className="text-base text-gray-700">
-                Twitter reputation: {twitterReputation.basicReputation}
-              </p>
-            ) : null}
-          </div>
-          <div className="px-4 pb-5 sm:p-6 flex flex-col">
-            <h2 className="text-xl mb-3 leading-6 font-medium text-gray-900">
-              Account Linking
-            </h2>
-            <div className="sm:flex sm:items-center sm:justify-between">
-              <div className="max-w-xl text-base text-gray-700">
-                <p>{!connected && "Please connect your wallet first."}</p>
-                <p>{!hasASession && "Please sign in to a web 2 account."}</p>
-                <p>
-                  {hasASession &&
-                    isCurrentAccountLinked &&
-                    "Your account is already linked to an Ethereum address."}
-                </p>
-                <p>
-                  {hasASession &&
-                    !isCurrentAccountLinked &&
-                    "Your account is not linked to any Ethereum address."}
-                </p>
-                <p>{accountLinkingMessage}</p>
-              </div>
-              <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
-                <button
-                  disabled={
-                    !connected ||
-                    !hasASession ||
-                    isCurrentAccountLinked ||
-                    !isOnProperNetwork
-                  }
-                  onClick={() => signAssociation()}
-                  type="button"
-                  className={`inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm  bg-blue-600 hover:bg-blue-700 disabled:bg-gray-300`}
-                >
-                  LINK ADDRESS
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="px-4 pb-5 sm:p-6 flex flex-col">
-            <h2 className="text-xl mb-3 leading-6 font-medium text-gray-900">
-              Badges
-            </h2>
-            {!connected && <p>Please connect your wallet first.</p>}
-            <h3 className="text-base text-blue-500">Twitter</h3>
-            {tokens.length > 0 &&
-              tokens.map((token) => (
-                <div
-                  key={token.idHash}
-                  className="sm:flex sm:items-center sm:justify-between mb-4"
-                >
-                  <div className="max-w-xl text-base text-gray-700">
-                    <div className="text-xs">
-                      <p>id: {token.idHash}</p>
-                      <p>status: {token.status}</p>
+            <div className="px-4 pb-5 sm:p-6 flex flex-col">
+              <h2 className="text-xl mb-3 leading-6 font-medium text-gray-900">
+                Badges
+              </h2>
+              {!connected && <p>Please connect your wallet first.</p>}
+              <h3 className="text-base text-blue-500">Twitter</h3>
+              {tokens.length > 0 &&
+                tokens.map((token) => (
+                  <div
+                    key={token.idHash}
+                    className="sm:flex sm:items-center sm:justify-between mb-4"
+                  >
+                    <div className="max-w-xl text-base text-gray-700">
+                      <div className="text-xs">
+                        <p>id: {token.idHash}</p>
+                        <p>status: {token.status}</p>
+                      </div>
+                    </div>
+                    <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
+                      <button
+                        disabled={token.status !== TokenStatus.MINTED}
+                        onClick={() =>
+                          burnToken(token.web2Provider, token.idHash)
+                        }
+                        type="button"
+                        className={`inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm  bg-blue-600 hover:bg-blue-700 disabled:bg-gray-300`}
+                      >
+                        BURN
+                      </button>
                     </div>
                   </div>
-                  <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
-                    <button
-                      disabled={token.status !== TokenStatus.MINTED}
-                      onClick={() =>
-                        burnToken(token.web2Provider, token.idHash)
-                      }
-                      type="button"
-                      className={`inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm  bg-blue-600 hover:bg-blue-700 disabled:bg-gray-300`}
-                    >
-                      BURN
-                    </button>
-                  </div>
-                </div>
-              ))}
+                ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
