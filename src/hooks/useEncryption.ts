eb07b84d433e5c01e4368e77785d529a9a9f9bf5
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-24 17:18:49 +0100

    add useEncryption hook

---
+++ src/hooks/useEncryption.ts
@@ -0,0 +1,24 @@
+import { useCallback, useState } from "react";
+import { ethers } from "ethers";
+
+const useEncryption = () => {
+  const [publicKey, setPublicKey] = useState<string | null>(null);
+
+  const getPublicKey = useCallback(async () => {
+    // @ts-ignore: ignore
+    await window.ethereum.enable();
+    // @ts-ignore: ignore
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.listAccounts();
+    const pubKey = await provider.send("eth_getEncryptionPublicKey", [
+      accounts[0],
+    ]);
+    setPublicKey(pubKey);
+
+    return pubKey;
+  }, []);
+
+  return { getPublicKey, publicKey };
+};
+
+export default useEncryption;
