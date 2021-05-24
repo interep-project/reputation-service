6e0896a936ec7359405a50286ec4875ad88f6101
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-24 17:55:59 +0100

    add salt to encryption

--- src/utils/crypto/encryption.ts
+++ src/utils/crypto/encryption.ts
@@ -9,3 +9,17 @@ export const encryptMessage = (publicKey: string, message: string): string =>
   stringifiableToHex(
     encrypt(publicKey, { data: message }, "x25519-xsalsa20-poly1305")
   );
+
+export const encryptMessageWithSalt = (
+  publicKey: string,
+  message: string
+): string => {
+  const salt = stringifiableToHex(ethers.utils.randomBytes(12));
+
+  const messageWithSalt = JSON.stringify({
+    message,
+    salt,
+  });
+
+  return encryptMessage(publicKey, messageWithSalt);
+};
