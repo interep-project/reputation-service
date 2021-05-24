b1d7876c78abbc041d96299bab7d5ae51d377ee9
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-24 17:12:01 +0100

    add createBackendAssociationMessage function

--- src/core/linking/signature.ts
+++ src/core/linking/signature.ts
@@ -2,7 +2,21 @@ export type AssociationMessageProps = {
   address: string;
   web2AccountId: string;
 };
+// TODO: Improve with more explicit message to sign
 export const createAssociationMessage = ({
   address,
   web2AccountId,
-}: AssociationMessageProps) => `InterRep:${address}:${web2AccountId}`;
+}: AssociationMessageProps): string => `InterRep:${address}:${web2AccountId}`;
+
+export type BackendAssociationMessageProps = {
+  address: string;
+  provider: string;
+  providerAccountId: string;
+};
+
+export const createBackendAssociationMessage = ({
+  address,
+  provider,
+  providerAccountId,
+}: BackendAssociationMessageProps): string =>
+  `InterRep:${address}:${provider}:${providerAccountId}`;
