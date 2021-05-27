1e4732fbae595ea9f87b3efbbfe60d3c0031efaa
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-05-27 10:32:25 +0100

    update backend attestation to include token id hash

--- src/core/signing/createBackendAttestationMessage.ts
+++ src/core/signing/createBackendAttestationMessage.ts
@@ -1,12 +1,20 @@
 export type BackendAttestationMessageParams = {
   address: string;
+  tokenIdHash: string;
   provider: string;
   providerAccountId: string;
 };
 
 export const createBackendAttestationMessage = ({
   address,
+  tokenIdHash,
   provider,
   providerAccountId,
 }: BackendAttestationMessageParams): string =>
-  `InterRep:${address}:${provider}:${providerAccountId}`;
+  JSON.stringify({
+    service: "InterRep",
+    tokenIdHash,
+    userAddress: address,
+    web2Provider: provider,
+    providerAccountId: providerAccountId,
+  });
