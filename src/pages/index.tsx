6c41acffb16d8e99d3fee2596847d77256bd3697
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-06-14 17:11:03 +0100

    move account linking instructions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,7 @@ import { getDefaultNetworkId } from "src/utils/crypto/getDefaultNetwork";
 import useEncryption from "src/hooks/useEncryption";
 import { getChecksummedAddress } from "src/utils/crypto/address";
 import DeployedContractSection from "src/components/DeployedContractSection/DeployedContractSection";
+import { getAccountLinkingInstruction } from "src/utils/frontend/getAccountLinkingInstruction";
 
 // TODO: create abstraction for calls to API and error handling
 const getMyTwitterReputation = async () => {
@@ -76,33 +77,6 @@ const mintToken = async (tokenId: string) => {
 //   name: "InterRep",
 //   chainId,
 // });
-
-const getAccountLinkingInstruction = ({
-  connected,
-  hasASession,
-  isCurrentAccountLinked,
-  hasEnoughReputation,
-}: {
-  connected: boolean;
-  hasASession: boolean;
-  isCurrentAccountLinked?: boolean;
-  hasEnoughReputation: boolean;
-}): string | null => {
-  if (!connected) return "Please connect your wallet first.";
-
-  if (!hasASession) return "Please sign in to a web 2 account.";
-
-  if (isCurrentAccountLinked)
-    return "Your account is already linked to an Ethereum address.";
-
-  if (!hasEnoughReputation)
-    return "Sorry, we were unable to confirm the reputation of your Twitter account.";
-
-  if (hasEnoughReputation)
-    return "Your account is not linked to any Ethereum address.";
-
-  return null;
-};
 
 export default function Home() {
   const [session] = useSession();
