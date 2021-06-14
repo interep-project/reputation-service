0833e12c851eb7d6ffb95013ab67260341c20b1b
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-06-14 15:33:47 +0100

    signin only with Twitter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -373,7 +373,9 @@ export default function Home() {
                   </div>
                   <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
                     <button
-                      onClick={() => (hasASession ? signOut() : signIn())}
+                      onClick={() =>
+                        hasASession ? signOut() : signIn("twitter")
+                      }
                       type="button"
                       className={`inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm ${
                         hasASession
