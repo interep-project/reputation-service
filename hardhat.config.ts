fd3f53a8b43cd3e395c84db6645c5296dc7cf41d
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-06-14 14:54:05 +0100

    update hh config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -49,7 +49,7 @@ const getNetworks = () => {
 };
 
 const config: HardhatUserConfig = {
-  defaultNetwork: defaultNetworkByEnv[process.env.NODE_ENV].name,
+  defaultNetwork: defaultNetworkByEnv[process.env.NODE_ENV]?.name || "hardhat",
   solidity: {
     version: "0.8.0",
   },
