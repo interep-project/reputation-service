b282dfea036d7c47103ac8bb74a122c9803584ba
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-06-14 17:12:46 +0100

    move spinner into own component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,7 @@ import { getDefaultNetworkId } from "src/utils/crypto/getDefaultNetwork";
 import useEncryption from "src/hooks/useEncryption";
 import { getChecksummedAddress } from "src/utils/crypto/address";
 import DeployedContractSection from "src/components/DeployedContractSection/DeployedContractSection";
+import Spinner from "src/components/Spinner/Spinner";
 import { getAccountLinkingInstruction } from "src/utils/frontend/getAccountLinkingInstruction";
 
 // TODO: create abstraction for calls to API and error handling
@@ -443,28 +444,7 @@ export default function Home() {
                             type="button"
                             className={`inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm  bg-blue-600 hover:bg-blue-700 disabled:bg-gray-300 disabled:cursor-default`}
                           >
-                            {isMintingInProgress && (
-                              <svg
-                                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                              >
-                                <circle
-                                  className="opacity-25"
-                                  cx="12"
-                                  cy="12"
-                                  r="10"
-                                  stroke="currentColor"
-                                  strokeWidth="4"
-                                ></circle>
-                                <path
-                                  className="opacity-75"
-                                  fill="currentColor"
-                                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                ></path>
-                              </svg>
-                            )}
+                            {isMintingInProgress && <Spinner />}
                             MINT
                           </button>
                         )}
