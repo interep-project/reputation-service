3f9fcfbd273e6be4674554a715f0d1c7b9c2a644
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-06-21 10:33:24 +0100

    update sample rate

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -17,5 +17,5 @@ Sentry.init({
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.7,
 });
