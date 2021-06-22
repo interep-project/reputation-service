54937a88b5855468598f68f5db61f3466e68f4c5
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-06-22 15:52:39 +0100

    add tests for checkLink

--- src/mocks/session.ts
+++ src/mocks/session.ts
@@ -3,6 +3,6 @@ import { Session } from "next-auth";
 export const mockSession: Session = {
   user: { name: "Joe" },
   expires: "123",
-  web2AccountId: "394223",
+  web2AccountId: "6087dabb0b3af8703a581bef",
   twitter: { userId: "12", username: "joe" },
 };
