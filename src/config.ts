694eea0706edb33dbcd6d43accfeb38fbd0eb2e8
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-08-06 14:24:18 +0100

    change default network

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ if (nextConfig && process.env.NODE_ENV && process.env.NODE_ENV !== "test") {
     throw new Error("Default network is not defined");
   }
 } else {
-  defaultNetwork = "kovan";
+  defaultNetwork = "ropsten";
 }
 
 export const onboardStyles = `'sha256-b9ziL8IJLxwegMVUIsr1Xonaxx9fkYcSKScBVxty+b8=' 'sha256-BgOP/ML7iRgKpqjhNfXke6AKgTv2KiGYPBitEng05zo=' 'sha256-WEkB3t+nfyfJBFAy7S1VN7AGcsstGkPtH/sEme5McoM=' 'sha256-1q71146AbRaIXMXEynPmTgkuaVhiUlRiha3g3LjARmY=' 'sha256-2HcLdA7hf+eZilVMBcZB4VFlx8PSxhoPt5BZCxc/85s=' 'sha256-WLOC06MT+Dqzsp7Cy382ZCzwQQ4P0EneoLyM/Yao4rw=' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=' 'sha256-pA817lKKQj1w2GkmEolfMacD1BU4+jNuNTl3cKD16f4=' 'sha256-NYlZ6i82DNnjefyMZyP17TIpq26QW0lg5yF6PDEOpYA=' 'sha256-nDu9nFngbf6yKkK8kNYFmU+5A6Ew5P34jdNXAyYFeFw=' 'sha256-NB1qv9+tZOZ0Iy3B+6Y04eDYY2YS+HLIBGfZ3o57ek0='`;
