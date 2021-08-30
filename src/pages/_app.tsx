bb0ca8a63e40fd9415ef7d40255f8865a2ba1de0
cedoor <me@cedoor.dev>
2021-08-30 14:12:41 +0200

    refactor: allow temporarily inline style-src

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import PropTypes from "prop-types";
 import { Provider as NextAuthProvider } from "next-auth/client";
 import { Web3ContextProvider } from "src/services/context/Web3Provider";
 import {
@@ -11,6 +12,7 @@ import {
 import "src/styles/globals.css";
 import "@fontsource/roboto";
 import React from "react";
+import Head from "next/head";
 
 const theme = createTheme({
   palette: {
@@ -38,15 +40,38 @@ const useStyles = makeStyles(() =>
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const classes = useStyles();
 
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <ThemeProvider theme={theme}>
-      <Web3ContextProvider>
-        <NextAuthProvider session={pageProps.session}>
-          <Paper className={classes.container} elevation={0} square={true}>
-            <Component {...pageProps} />
-          </Paper>
-        </NextAuthProvider>
-      </Web3ContextProvider>
-    </ThemeProvider>
+    <React.Fragment>
+      <Head>
+        <title>InterRep</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Web3ContextProvider>
+          <NextAuthProvider session={pageProps.session}>
+            <Paper className={classes.container} elevation={0} square={true}>
+              <Component {...pageProps} />
+            </Paper>
+          </NextAuthProvider>
+        </Web3ContextProvider>
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
-import crypto from "crypto";
 import { ServerStyleSheets } from "@material-ui/core";
+import crypto from "crypto";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import React from "react";
 
 const cspHashOf = (text: string): string => {
@@ -9,18 +9,18 @@ const cspHashOf = (text: string): string => {
   return `'sha256-${hash.digest("base64")}'`;
 };
 
-const onboardStyles = `'sha256-b9ziL8IJLxwegMVUIsr1Xonaxx9fkYcSKScBVxty+b8=' 'sha256-BgOP/ML7iRgKpqjhNfXke6AKgTv2KiGYPBitEng05zo=' 'sha256-WEkB3t+nfyfJBFAy7S1VN7AGcsstGkPtH/sEme5McoM=' 'sha256-1q71146AbRaIXMXEynPmTgkuaVhiUlRiha3g3LjARmY=' 'sha256-2HcLdA7hf+eZilVMBcZB4VFlx8PSxhoPt5BZCxc/85s=' 'sha256-WLOC06MT+Dqzsp7Cy382ZCzwQQ4P0EneoLyM/Yao4rw=' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=' 'sha256-pA817lKKQj1w2GkmEolfMacD1BU4+jNuNTl3cKD16f4=' 'sha256-NYlZ6i82DNnjefyMZyP17TIpq26QW0lg5yF6PDEOpYA=' 'sha256-nDu9nFngbf6yKkK8kNYFmU+5A6Ew5P34jdNXAyYFeFw=' 'sha256-NB1qv9+tZOZ0Iy3B+6Y04eDYY2YS+HLIBGfZ3o57ek0='`;
+//const onboardStyles = `'sha256-b9ziL8IJLxwegMVUIsr1Xonaxx9fkYcSKScBVxty+b8=' 'sha256-BgOP/ML7iRgKpqjhNfXke6AKgTv2KiGYPBitEng05zo=' 'sha256-WEkB3t+nfyfJBFAy7S1VN7AGcsstGkPtH/sEme5McoM=' 'sha256-1q71146AbRaIXMXEynPmTgkuaVhiUlRiha3g3LjARmY=' 'sha256-2HcLdA7hf+eZilVMBcZB4VFlx8PSxhoPt5BZCxc/85s=' 'sha256-WLOC06MT+Dqzsp7Cy382ZCzwQQ4P0EneoLyM/Yao4rw=' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=' 'sha256-pA817lKKQj1w2GkmEolfMacD1BU4+jNuNTl3cKD16f4=' 'sha256-NYlZ6i82DNnjefyMZyP17TIpq26QW0lg5yF6PDEOpYA=' 'sha256-nDu9nFngbf6yKkK8kNYFmU+5A6Ew5P34jdNXAyYFeFw=' 'sha256-NB1qv9+tZOZ0Iy3B+6Y04eDYY2YS+HLIBGfZ3o57ek0='`;
 
 export default class MyDocument extends Document {
   render() {
-    const nonce = crypto.randomBytes(8).toString("base64");
+    //const nonce = crypto.randomBytes(8).toString("base64");
 
-    let csp = `default-src 'self'; img-src 'self' data:; style-src 'self' 'nonce-${nonce}' ${onboardStyles}; script-src 'self' ${cspHashOf(
+    let csp = `default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'self' ${cspHashOf(
       NextScript.getInlineScriptSource(this.props)
     )}; `;
 
     if (process.env.NODE_ENV !== "production") {
-      csp = `style-src 'self' 'unsafe-inline'; font-src 'self' data:; default-src 'self'; script-src 'unsafe-eval' 'self' ${cspHashOf(
+      csp = `style-src 'self' 'unsafe-inline'; font-src 'self' data:; default-src 'self'; img-src 'self' data:; script-src 'unsafe-eval' 'self' ${cspHashOf(
         NextScript.getInlineScriptSource(this.props)
       )}; `;
     }
@@ -29,7 +29,7 @@ export default class MyDocument extends Document {
 
     return (
       <Html>
-        <Head nonce={nonce}>
+        <Head>
           <meta httpEquiv="Content-Security-Policy" content={csp} />
         </Head>
         <body>
@@ -41,7 +41,32 @@ export default class MyDocument extends Document {
   }
 }
 
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with server-side generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  // Render app and page and get the context of the page with collected side effects.
   const sheets = new ServerStyleSheets();
   const originalRenderPage = ctx.renderPage;
 
@@ -54,6 +79,7 @@ MyDocument.getInitialProps = async (ctx) => {
 
   return {
     ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
     styles: [
       ...React.Children.toArray(initialProps.styles),
       sheets.getStyleElement(),
