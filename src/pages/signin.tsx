6e8ab2c0e9d7ed4fae7bd7a84703779b603b3c37
cedoor <me@cedoor.dev>
2021-09-27 18:58:15 +0200

    feat: create Signin page

---
+++ src/pages/signin.tsx
@@ -0,0 +1,53 @@
+import { Button, Heading, VStack, Icon, Tooltip } from "@chakra-ui/react"
+import { FaGithub, FaRedditAlien, FaTwitter, FaInfoCircle } from "react-icons/fa"
+import { signIn, useSession } from "next-auth/client"
+import React from "react"
+
+export default function Signin(): JSX.Element {
+    const [session] = useSession()
+
+    return (
+        <>
+            <Heading as="h2" mb="30px" size="xl">
+                Web2 Login
+                <Tooltip
+                    label="Sign in with one of our supported Web2 providers to obtain your reputation."
+                    placement="right-start"
+                >
+                    <span>
+                        <Icon boxSize="20px" ml="10px" mb="5px" color="background.200" as={FaInfoCircle} />
+                    </span>
+                </Tooltip>
+            </Heading>
+            <VStack spacing={4} align="left">
+                <Button
+                    onClick={() => signIn("twitter")}
+                    leftIcon={<FaTwitter />}
+                    colorScheme="twitter"
+                    variant="ghost"
+                    isDisabled={!!session}
+                >
+                    Twitter
+                </Button>
+                <Button
+                    onClick={() => signIn("github")}
+                    leftIcon={<FaGithub />}
+                    colorScheme="github"
+                    variant="ghost"
+                    isDisabled={!!session}
+                >
+                    Github
+                </Button>
+                <Button
+                    onClick={() => signIn("reddit")}
+                    leftIcon={<FaRedditAlien />}
+                    colorScheme="reddit"
+                    variant="ghost"
+                    isDisabled={!!session}
+                >
+                    Reddit
+                </Button>
+            </VStack>
+        </>
+    )
+}
