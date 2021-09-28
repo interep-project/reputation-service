0b9255d8ba6bd1d9fab27fc479b24802876095c3
cedoor <me@cedoor.dev>
2021-09-28 20:28:22 +0200

    feat: add semisolid button variant

--- src/styles/components/Button.ts
+++ src/styles/components/Button.ts
@@ -22,16 +22,31 @@ const Button = {
                 _active: { bg: hoverBg }
             }
         },
-        ghost: (props: StyleFunctionProps): SystemStyleObject => {
+        link: (): SystemStyleObject => ({
+            _hover: {
+                textDecoration: "none"
+            }
+        }),
+        semisolid: (props: StyleFunctionProps): SystemStyleObject => {
             const { colorScheme: c, theme } = props
-            const bg = mode(`${c}.50`, transparentize(`${c}.200`, 0.05)(theme))(props)
+
+            const darkBg = transparentize(`${c}.200`, 0.05)(theme)
+            const darkHoverBg = transparentize(`${c}.200`, 0.12)(theme)
+            const darkActiveBg = transparentize(`${c}.200`, 0.24)(theme)
+
+            const bg = mode(`${c}.50`, darkBg)(props)
 
             return {
+                color: mode(`${c}.600`, `${c}.200`)(props),
                 bg,
                 _hover: {
+                    bg: darkHoverBg,
                     _disabled: {
                         bg
                     }
+                },
+                _active: {
+                    bg: mode(`${c}.100`, darkActiveBg)(props)
                 }
             }
         }
