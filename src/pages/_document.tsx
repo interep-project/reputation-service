5ef6f9c38b8bb723c93451e50c94a4735a8da9a3
cedoor <me@cedoor.dev>
2021-09-29 19:19:35 +0200

    feat: add poap api url to connect-src csp rules

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,7 +25,7 @@ const generateCSP = (): string => {
         "img-src": `'self' data:`,
         "style-src": `'self' 'unsafe-inline'`,
         "script-src": `'self' 'unsafe-inline' 'unsafe-eval'`,
-        "connect-src": `'self' *.sentry.io`
+        "connect-src": `'self' *.sentry.io https://api.poap.xyz`
     }
 
     // Convert to string and return.
