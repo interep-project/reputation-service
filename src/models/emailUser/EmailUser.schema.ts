10b981a125a4a04e4a1b96bb41e6e83f02f3c803
neilhacker <39994403+neilhacker@users.noreply.github.com>
2021-11-03 13:02:02 +0000

    feature: added email model to src folder

---
+++ src/models/emailUser/EmailUser.schema.ts
@@ -0,0 +1,17 @@
+import { Schema } from "mongoose"
+import { findByHashId } from "./EmailUser.statics"
+import { EmailUserData, EmailUserDocument, EmailUserModel } from "./EmailUser.types"
+
+const EmailUserSchemaFields: Record<keyof EmailUserData, any> = {
+    provider: { type: String, required: true },
+    hashId: { type: String, required: true, index: true, unique: true },
+    verified: {type: Boolean, required: true},
+    joined: { type: Boolean, required: true },
+    emailRandomToken: { type: String, required: true }
+}
+
+const EmailUserSchema = new Schema<EmailUserDocument, EmailUserModel>(EmailUserSchemaFields)
+
+EmailUserSchema.statics.findByHashId = findByHashId
+
+export default EmailUserSchema
