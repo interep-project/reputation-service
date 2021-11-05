4b006b1b37225905ad6ed6462444c7c3e82cf03b
neilhacker <39994403+neilhacker@users.noreply.github.com>
2021-11-05 15:17:27 +0000

    feature: email tests updated

--- src/tests/api/email.test.ts
+++ src/tests/api/email.test.ts
@@ -1,7 +1,24 @@
-import getReputation from "src/pages/api/reputation/[web2Provider]/[username]"
 import { clearDatabase, connect, dropDatabaseAndDisconnect } from "src/utils/backend/testDatabase"
+import EmailUser from "../../models/emailUser/EmailUser.model"
+import handler from "src/pages/api/email/sendEmail"
+import verifyHandler from "src/pages/api/email/verifyEmail"
+
 import createNextMocks from "src/mocks/createNextMocks"
-import { ReputationLevel, Web2Provider } from "@interrep/reputation-criteria"
+// import fetchMock from "jest-fetch-mock";
+
+import config from "src/config"
+
+import linkAccounts from "src/core/email/sendEmail"
+
+const linkAccountsMocked = linkAccounts as jest.MockedFunction<typeof linkAccounts>
+
+const bodyParams = {
+    accountId: "6087dabb0b3af8703a581bef",
+    address: "0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7",
+    userSignature: "0xSignature",
+    userPublicKey: "publicKey"
+}
+
 
 describe("Email verification APIs", () => {
     beforeAll(async () => {
@@ -19,55 +36,63 @@ describe("Email verification APIs", () => {
     describe("Get user reputation", () => {
 
         // return error if no email address given
-        it("Should return a 405 if the method is not GET", async () => {
-            const requestOptions = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ 
-                    address: "" })
-            };
-            const response = await fetch('/api/email/sendEmail', requestOptions);
-            const data = await response.json();
-
-            expect(data._getStatusCode()).toBe(405)
-        })
-
-        // reject email address not from @hotmail
-        it("Should return a 405 if the method is not GET", async () => {
-            const requestOptions = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ 
-                    address: "test@outlook.com" })
-            };
-            const response = await fetch('/api/email/sendEmail', requestOptions);
-            const data = await response.json();
-
-            expect(data._getStatusCode()).toBe(405)
-        })
-
-        it("Should return a 405 if the method is not GET", async () => {
+        it("Should return error 405 if the no address", async () => {
             const { req, res } = createNextMocks({
-                query: {},
-                method: "PUT"
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: { address: "" }
             })
-
-            await getReputation(req, res)
-
+    
+            await handler(req, res)
             expect(res._getStatusCode()).toBe(405)
         })
 
-        it("Should return the user reputation", async () => {
-            const web2Provider = Web2Provider.TWITTER
-            const username = "username"
-
+        // // reject email address not from @hotmail
+        it("Should return error 405 if wrong type of email entered", async () => {
             const { req, res } = createNextMocks({
-                query: { web2Provider, username }
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: { address: "test@outlook.com" }
+            })
+    
+            await handler(req, res)
+            expect(res._getStatusCode()).toBe(405)
+        })
+
+        it("Should return 200 if correct type of email entered (@hotmail) ", async () => {
+            const { req, res } = createNextMocks({
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: { address: "test@hotmail.com" }
+            })
+    
+            await handler(req, res)
+            expect(res._getStatusCode()).toBe(200)
+        })
+
+        it("Should return 200 if correct type of email entered (@hotmail) ", async () => {
+            const { req, res } = createNextMocks({
+                query: {id: "="+"1234"+"?email="+"test@hotmail.co.uk"},
+                method: "PUT",
             })
 
-            await getReputation(req, res)
+            const account = new EmailUser({
+                provider: "hotmail",
+                hashId: "test@hotmail.co.uk",
+                verified: false,
+                joined: false,
+                emailRandomToken: "1234"
+            })
 
-            expect(res._getData().data.reputation).toBe(ReputationLevel.GOLD)
+            await account.save()
+
+            // const link = config.HOST+"/api/email/verifyEmail?id="+"1234"+"?email="+"test@hotmail.co.uk";
+            
+            await verifyHandler(req, res)
+            expect(res._getStatusCode()).toBe(200)
         })
+
+
+
     })
 })
