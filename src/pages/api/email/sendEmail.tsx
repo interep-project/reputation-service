b7284ff80b391a53a7e5eb1d2c2f7daec4a8665a
Geoff Lamperd <glamperd@gmail.com>
2021-11-08 17:16:19 +1100

    fix: email tests

--- src/pages/api/email/sendEmail.tsx
+++ src/pages/api/email/sendEmail.tsx
@@ -4,13 +4,16 @@
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next"
 import { withSentry } from "@sentry/nextjs"
 import createEmailAccount from "src/core/email/createEmailAccount"
+import logger from "src/utils/backend/logger"
 
 
 async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
 
 	console.log("**********Checking address & sending email************")
+	logger.silly(`Request: ${req.body}`)
 
-	const userEmail = req.body.address
+	const { address } = JSON.parse(req.body)
+	const userEmail = address
 	console.log(userEmail)
 	console.log("Email address: ",userEmail)
 	var message
@@ -18,8 +21,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void>
 	// -------------------checking email format-----------------
 	if(!userEmail || userEmail.includes("@hotmail") != true){
 		message = "invalid email, must be an @hotmail address"
-        res.status(402).json({ message })
-		return
+        return res.status(402).json({ message })
 	}
 
 	// -------------------checking user is new-----------------
@@ -27,7 +29,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void>
 		console.log("trying to make account")
 		await createEmailAccount(userEmail, "hotmail").then((message) => {
 			console.log("createEmailAccount message", message)
-			res.status(200).json({message})
+			return res.status(200).json({message})
 		})
 
 	} catch (err) {
