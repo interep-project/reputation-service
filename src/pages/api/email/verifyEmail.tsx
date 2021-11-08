cb0f948e4096d83d594a61393b121b4093418af7
neilhacker <39994403+neilhacker@users.noreply.github.com>
2021-11-08 10:18:43 +0000

    fix: used config.HOST vs hardcoded variable in verifyEmail api

--- src/pages/api/email/verifyEmail.tsx
+++ src/pages/api/email/verifyEmail.tsx
@@ -1,10 +1,11 @@
-import {verifyUser } from  '../../../utils/email/mongo_verify_user';
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next"
 import { withSentry } from "@sentry/nextjs"
 import verifyEmailAccount from "src/core/email/verifyEmailAccount"
+import config from "src/config"
 
 async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
 
+	console.log(req)
 	let id = (req.query.id as string)
 
 	// id comes in form like 6557?email=johnsmith@hotmail.co.uk
@@ -13,7 +14,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void>
 	console.log("incoming verification link stats")
 
 	// this url will need to be replaced
-	if(("http://"+req.headers.host)==("http://localhost:3000")) {
+	if(("http://"+req.headers.host)==(config.HOST)) {
 		console.log("Domain is matched. Information is from Authentic email");
 		console.log(req.query.id)
 
@@ -21,15 +22,16 @@ async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void>
 			console.log("trying to make account")
 			await verifyEmailAccount(email, rand).then((message) => {
 				console.log("createEmailAccount message", message)
-				res.end(message)
+				res.status(200).end(message)
 			})
 	
 		} catch (err) {
 			console.log(err)
+			res.status(401).end(err)
 		}
 		
 	} else {
-		res.end("Request is from unknown source");
+		res.status(402).end("Request is from unknown source");
 	}
 };
 
