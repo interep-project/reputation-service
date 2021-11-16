7599d5fefdc2e4322cb48884d769d29b1d3eb833
cedoor <me@cedoor.dev>
2021-11-16 16:58:15 +0100

    fix: parse body content in api to send mail

--- src/pages/api/email/sendEmail.tsx
+++ src/pages/api/email/sendEmail.tsx
@@ -6,50 +6,37 @@ import { withSentry } from "@sentry/nextjs"
 import createEmailAccount from "src/core/email/createEmailAccount"
 import logger from "src/utils/backend/logger"
 
-
 async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+    const userEmail = JSON.parse(req.body).address
 
-	logger.silly("**********Checking address & sending email************")
-	logger.silly(`Request: ${req.body}`)
+    console.log("Email address: ", userEmail)
 
-	// console.log("req", req)
-	console.log("req body", req)
+    let message
 
-	// console.log("req body", JSON.parse(req.body)["address"])
-	console.log("req body", req.body["address"])
+    try {
+        // -------------------checking email format-----------------
+        if (!userEmail || !userEmail.includes("@hotmail")) {
+            message = "invalid email, must be an @hotmail address"
 
-	// const userEmail = JSON.parse(req.body)["address"]
-	const userEmail = req.body["address"]
+            res.status(402).json({ message })
+        } else {
+            // user has valid hotmail account
+            try {
+                logger.silly("trying to make account")
+                await createEmailAccount(userEmail, "hotmail").then((message) => {
+                    console.log("createEmailAccount message", message)
 
-	// console.log("req", req)
-	console.log(userEmail)
-	console.log("Email address: ",userEmail)
-	var message
+                    res.status(200).json({ message })
+                })
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    } catch (err) {
+        message = "invalid email, must be an @hotmail address"
 
-	try {
-		// -------------------checking email format-----------------
-		if(!userEmail || userEmail.includes("@hotmail") != true){
-			message = "invalid email, must be an @hotmail address"
-			res.status(402).json({ message })
-		} else {
-			// user has valid hotmail account
-			try {
-				logger.silly("trying to make account")
-				await createEmailAccount(userEmail, "hotmail").then((message) => {
-					console.log("createEmailAccount message", message)
-					res.status(200).json({message})
-				})
-		
-			} catch (err) {
-				console.log(err)
-			}
-		
-		}
-	} catch (err) {
-		message = "invalid email, must be an @hotmail address"
-		res.status(402).json({ message })
-	}
-
+        res.status(402).json({ message })
+    }
 }
 
 export default withSentry(handler as NextApiHandler)
