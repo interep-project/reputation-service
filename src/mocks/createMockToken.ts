db6bfa35f271cecb78284504d27bf637ab047e8d
cedoor <me@cedoor.dev>
2021-11-29 17:47:08 +0100

    refactor: update mocks functions

--- src/mocks/createMockToken.ts
+++ src/mocks/createMockToken.ts
@@ -1,16 +1,16 @@
 import { OAuthProvider } from "@interrep/reputation-criteria"
 import { TokenDocument, TokenStatus } from "@interrep/db"
 
-const createMockTokenObject = (override?: Partial<TokenDocument>) => ({
-    contractAddress: "0x8398bCD4f633C72939F9043dB78c574A91C99c0A",
-    userAddress: "0xBf493eD72eE6017F5b0dD02AaC0EAC41a735b182",
-    issuanceTimestamp: Date.now(),
-    provider: OAuthProvider.TWITTER,
-    chainId: 33137,
-    status: TokenStatus.NOT_MINTED,
-    decimalId: "99841047636891000514743136290083633803587659719163489038083863229813808784161",
-    encryptedAttestation: "encryptedAttestation",
-    ...override
-})
-
-export default createMockTokenObject
+export default function createMockTokenObject(override?: Partial<TokenDocument>) {
+    return {
+        contractAddress: "0x8398bCD4f633C72939F9043dB78c574A91C99c0A",
+        userAddress: "0xBf493eD72eE6017F5b0dD02AaC0EAC41a735b182",
+        issuanceTimestamp: Date.now(),
+        provider: OAuthProvider.TWITTER,
+        chainId: 33137,
+        status: TokenStatus.NOT_MINTED,
+        decimalId: "99841047636891000514743136290083633803587659719163489038083863229813808784161",
+        encryptedAttestation: "encryptedAttestation",
+        ...override
+    }
+}
--- src/mocks/createNextMocks.ts
+++ src/mocks/createNextMocks.ts
@@ -1,14 +1,13 @@
 import { createMocks, Mocks, RequestMethod, RequestOptions } from "node-mocks-http"
 import { NextApiRequest, NextApiResponse } from "next"
 
-const createNextMocks = (reqOptions?: RequestOptions): Mocks<NextApiRequest, NextApiResponse> => {
+export default function createNextMocks(reqOptions?: RequestOptions): Mocks<NextApiRequest, NextApiResponse> {
     const { req, res } = createMocks<NextApiRequest, NextApiResponse>({
         method: "GET" as RequestMethod,
         ...reqOptions
     })
+
     req.body = JSON.stringify(req.body)
 
     return { req, res }
 }
-
-export default createNextMocks
