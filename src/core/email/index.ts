23c6b59a13c3f20182ec7e44dd0898040eadb797
cedoor <me@cedoor.dev>
2021-11-30 10:56:24 +0100

    style: fix prettier rules

--- src/core/email/index.ts
+++ src/core/email/index.ts
@@ -1,6 +1,6 @@
-import sendEmail from "./sendEmail";
-import EmailDomains from "./emailDomains";
-import checkEmailAddress from "./checkEmailAddress";
-import createEmailAccount from "./createEmailAccount";
+import sendEmail from "./sendEmail"
+import EmailDomains from "./emailDomains"
+import checkEmailAddress from "./checkEmailAddress"
+import createEmailAccount from "./createEmailAccount"
 
 export { sendEmail, EmailDomains, checkEmailAddress, createEmailAccount }
