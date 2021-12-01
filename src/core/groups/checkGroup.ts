499f88ffc63ce51a20857c8e0b43750fb1dd2d24
cedoor <me@cedoor.dev>
2021-12-01 22:50:42 +0100

    fix: add correct check for email group names

--- src/core/groups/checkGroup.ts
+++ src/core/groups/checkGroup.ts
@@ -1,10 +1,17 @@
 import { getReputationLevels, ReputationLevel } from "@interrep/reputation-criteria"
 import { getPoapEvents, PoapEvent } from "src/core/poap"
 import { Provider, Web3Provider } from "src/types/groups"
+import { getEmailDomains } from "../email"
 
 export default function checkGroup(provider: Provider, name: ReputationLevel | PoapEvent | string): boolean {
-    if (provider === "telegram" || provider === "email") {
+    if (provider === "telegram") {
         return true
+    }
+
+    if (provider === "email") {
+        const emailDomains = getEmailDomains()
+
+        return emailDomains.indexOf(name) !== -1
     }
 
     if (provider === Web3Provider.POAP) {
