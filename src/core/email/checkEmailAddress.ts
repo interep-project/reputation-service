1154255c3cc3d6ae9a4c090b9dbe0dcff1d7f580
neilhacker <39994403+neilhacker@users.noreply.github.com>
2021-12-02 11:21:27 +0000

    bug: fixed email address checker to include regex to make sure form was correct first

--- src/core/email/checkEmailAddress.ts
+++ src/core/email/checkEmailAddress.ts
@@ -3,11 +3,14 @@ import EmailDomain from "./emailDomain"
 export default function checkEmailAddress(email: string) {
     const groupId = []
 
-    for (const [key, value] of Object.entries(EmailDomain)) {
-        if (email.includes(value)) {
-            groupId.push(key)
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+    {
+        for (const [key, value] of Object.entries(EmailDomain)) {
+            if (email.includes(value)) {
+                groupId.push(key)
+            }
         }
-    }
+    }   
 
     return groupId
 }
