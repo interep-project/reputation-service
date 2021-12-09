410ede1804ce016bf1b947ee2aeacc64d065b483
cedoor <me@cedoor.dev>
2021-12-09 18:20:53 +0100

    refactor: move telegram bot script to next tasks

---
+++ src/tasks/telegramBot.ts
@@ -0,0 +1,19 @@
+import { InterRepBot } from "@interrep/telegram-bot"
+import logger from "src/utils/backend/logger"
+
+export async function run() {
+    const { TELEGRAM_BOT_TOKEN, MONGO_URL, NEXTAUTH_URL } = process.env
+
+    if (!TELEGRAM_BOT_TOKEN || !MONGO_URL || !NEXTAUTH_URL) {
+        logger.warn("Telegram bot env variables are not defined. The bot will not be started.")
+        return
+    }
+
+    try {
+        const bot = new InterRepBot(TELEGRAM_BOT_TOKEN, MONGO_URL, NEXTAUTH_URL)
+
+        await bot.start()
+    } catch (error) {
+        logger.error(error)
+    }
+}
