bdda570bed914f68fbe0a0bc191fb81ea636f279
cedoor <me@cedoor.dev>
2021-12-23 11:34:22 +0100

    chore: update sentry project

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,13 +7,11 @@ import * as Sentry from "@sentry/nextjs"
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-    dsn: SENTRY_DSN || "https://4e55ead92ea6461f97488558918f8b5d@o862551.ingest.sentry.io/5821939",
+    dsn: SENTRY_DSN || "https://d1ee5a89bc854e9c95b8228eb2b72377@o1097611.ingest.sentry.io/6119210",
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1.0
+    // ...
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
     // that it will also get attached to your source maps
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 0.7
 })
