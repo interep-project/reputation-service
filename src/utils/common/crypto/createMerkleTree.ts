03cc6226fe38e2082a26114eea8358c25a8b5dc3
cedoor <me@cedoor.dev>
2022-01-03 17:24:19 +0100

    docs: add comments to crypto functions

--- src/utils/common/crypto/createMerkleTree.ts
+++ src/utils/common/crypto/createMerkleTree.ts
@@ -2,6 +2,10 @@ import { MerkleTree } from "@interrep/merkle-tree"
 import config from "src/config"
 import poseidon from "./poseidon"
 
+/**
+ * Creates a Merkle tree with static tree depth and zero value.
+ * @returns The Merkle tree instance.
+ */
 export default function createMerkleTree(): MerkleTree {
     return new MerkleTree((nodes) => poseidon(...nodes), config.MERKLE_TREE_DEPTH, BigInt(0))
 }
--- src/utils/common/crypto/poseidon.ts
+++ src/utils/common/crypto/poseidon.ts
@@ -1,5 +1,10 @@
 import { poseidon as _poseidon } from "circomlibjs"
 
+/**
+ * Creates a Poseidon big number hash.
+ * @param values The list of values to hash.
+ * @returns The big number hash.
+ */
 export default function poseidon(...values: string[] | number[] | bigint[]): string {
     values = values.map(BigInt)
 
