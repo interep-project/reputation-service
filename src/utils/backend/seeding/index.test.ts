0b9eac019b30b7f7c1efc1727ca92e5f6d02dd9b
cedoor <me@cedoor.dev>
2022-01-03 15:04:26 +0100

    test: add tests for seeding functions

---
+++ src/utils/backend/seeding/index.test.ts
@@ -0,0 +1,62 @@
+import { MerkleTreeZero, OAuthAccount } from "@interrep/db"
+import { OAuthProvider, ReputationLevel } from "@interrep/reputation"
+import config from "src/config"
+import { seedTwitterUsers } from "."
+import { connectDatabase, dropDatabaseAndDisconnect } from "../testDatabase"
+import seedZeroHashes from "./seedZeroHashes"
+
+jest.mock("src/services/twitter", () => ({
+    __esModule: true,
+    getTwitterUserByUsername: jest.fn(({ username }: any) => ({
+        id: username.length
+    }))
+}))
+
+describe("# utils/backend/seeding", () => {
+    beforeAll(async () => {
+        await connectDatabase()
+    })
+
+    afterAll(async () => {
+        await dropDatabaseAndDisconnect()
+    })
+
+    describe("# seedZeroHashes", () => {
+        it(`Should add ${config.MERKLE_TREE_DEPTH} zero hashes in the db`, async () => {
+            await seedZeroHashes()
+
+            const expectedValue = await MerkleTreeZero.countDocuments()
+
+            expect(expectedValue).toBe(config.MERKLE_TREE_DEPTH)
+        })
+
+        it("Should not add any zero hash if they are already there", async () => {
+            await seedZeroHashes()
+
+            const expectedValue = await MerkleTreeZero.countDocuments()
+
+            expect(expectedValue).toBe(config.MERKLE_TREE_DEPTH)
+        })
+    })
+
+    describe("# seedTwitterUsers", () => {
+        it(`Should add two Twitter users in the db`, async () => {
+            await seedTwitterUsers(["elonmusk", "vitalikButerin"])
+
+            const expectedValue1 = await OAuthAccount.countDocuments()
+            const expectedValue2 = await OAuthAccount.findByProviderAccountId(OAuthProvider.TWITTER, "8")
+
+            expect(expectedValue1).toBe(2)
+            expect(expectedValue2).not.toBeNull()
+            expect(expectedValue2?.reputation).toBe(ReputationLevel.GOLD)
+        })
+
+        it("Should not add any Twitter user if they are already there", async () => {
+            await seedTwitterUsers(["elonmusk", "vitalikButerin"])
+
+            const expectedValue = await OAuthAccount.countDocuments()
+
+            expect(expectedValue).toBe(2)
+        })
+    })
+})
