b655aa29de69b30535032f6e166f2ba447c027ab
cedoor <me@cedoor.dev>
2022-01-10 16:18:19 +0100

    fix: move twitter client definition within API fun

--- src/services/twitter/index.ts
+++ src/services/twitter/index.ts
@@ -2,11 +2,6 @@ import config from "src/config"
 import { TwitterUser } from "src/types/twitter"
 import Twitter from "twitter-v2"
 
-const client = new Twitter({
-    consumer_key: config.TWITTER_CONSUMER_KEY || "",
-    consumer_secret: config.TWITTER_CONSUMER_SECRET || ""
-})
-
 export const getTwitterUserByToken = async (token: string) => {
     const headers = new Headers({
         Authorization: token
@@ -20,6 +15,15 @@ export const getTwitterUserByToken = async (token: string) => {
 }
 
 export const getTwitterUserByUsername = async ({ username }: { username: string }): Promise<TwitterUser> => {
+    if (!config.TWITTER_CONSUMER_KEY || !config.TWITTER_CONSUMER_SECRET) {
+        throw new Error("Please define your Twitter environment variable inside .env")
+    }
+
+    const client = new Twitter({
+        consumer_key: config.TWITTER_CONSUMER_KEY,
+        consumer_secret: config.TWITTER_CONSUMER_SECRET
+    })
+
     // https://developer.twitter.com/en/docs/twitter-api/users/lookup/api-reference/get-users-by-username-username
     const { data } = await client.get(`users/by/username/${username}`, {
         "user.fields": ["id", "profile_image_url", "public_metrics", "verified", "created_at"]
