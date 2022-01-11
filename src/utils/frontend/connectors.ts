741be5f907fbff8fb535688d35d018af755f82c2
cedoor <me@cedoor.dev>
2022-01-11 14:41:43 +0100

    chore: ignore file for jest coverage

--- src/utils/frontend/connectors.ts
+++ src/utils/frontend/connectors.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import { InjectedConnector } from "@web3-react/injected-connector"
 
 export const injectedConnector = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] })
