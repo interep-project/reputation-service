d146d8f0d93aeb3351d993e69eeafb97e7947925
Eva <eva@hpcnt.com>
2022-02-24 15:59:49 +0900

    feat: add brightId onchain page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,72 +13,106 @@ export default function Providers(): JSX.Element {
     const { isOpen, onOpen, onClose } = useDisclosure()
 
     return (
-        <Container flex="1" mb="80px" mt="180px" px="80px" maxW="container.lg">
-            <Heading as="h2" mb="30px" size="xl">
-                Providers
-                <Tooltip label="Select one of our supported providers to join a group." placement="right-start">
-                    <span>
-                        <Icon
-                            boxSize="20px"
-                            ml="10px"
-                            mb="5px"
-                            color={colorMode === "light" ? "gray.500" : "background.200"}
-                            as={FaInfoCircle}
-                        />
-                    </span>
-                </Tooltip>
-            </Heading>
-            <HStack spacing={4} align="left">
-                <Button
-                    onClick={() => signIn("twitter")}
-                    leftIcon={<FaTwitter />}
-                    colorScheme="twitter"
-                    variant="semisolid"
-                    isDisabled={!!session}
-                >
-                    Twitter
-                </Button>
-                <Button
-                    onClick={() => signIn("github")}
-                    leftIcon={<FaGithub />}
-                    colorScheme="github"
-                    variant="semisolid"
-                    isDisabled={!!session}
-                >
-                    Github
-                </Button>
-                <Button
-                    onClick={() => signIn("reddit")}
-                    leftIcon={<FaRedditAlien />}
-                    colorScheme="reddit"
-                    variant="semisolid"
-                    isDisabled={!!session}
-                >
-                    Reddit
-                </Button>
+        <>
+            <Container flex="1" mb="80px" mt="180px" px="80px" maxW="container.lg">
+                <Heading as="h2" mb="30px" size="xl">
+                    Providers
+                    <Tooltip
+                        label="Select one of our supported providers to join a off-chain group."
+                        placement="right-start"
+                    >
+                        <span>
+                            <Icon
+                                boxSize="20px"
+                                ml="10px"
+                                mb="5px"
+                                color={colorMode === "light" ? "gray.500" : "background.200"}
+                                as={FaInfoCircle}
+                            />
+                        </span>
+                    </Tooltip>
+                </Heading>
+                <HStack spacing={4} align="left">
+                    <Button
+                        onClick={() => signIn("twitter")}
+                        leftIcon={<FaTwitter />}
+                        colorScheme="twitter"
+                        variant="semisolid"
+                        isDisabled={!!session}
+                    >
+                        Twitter
+                    </Button>
+                    <Button
+                        onClick={() => signIn("github")}
+                        leftIcon={<FaGithub />}
+                        colorScheme="github"
+                        variant="semisolid"
+                        isDisabled={!!session}
+                    >
+                        Github
+                    </Button>
+                    <Button
+                        onClick={() => signIn("reddit")}
+                        leftIcon={<FaRedditAlien />}
+                        colorScheme="reddit"
+                        variant="semisolid"
+                        isDisabled={!!session}
+                    >
+                        Reddit
+                    </Button>
 
-                <Button
-                    onClick={onOpen}
-                    leftIcon={<FaEnvelope />}
-                    colorScheme="green"
-                    variant="semisolid"
-                    isDisabled={isOpen}
-                >
-                    Email
-                </Button>
-                <EmailInputModal isOpen={isOpen} onClose={onClose} />
+                    <Button
+                        onClick={onOpen}
+                        leftIcon={<FaEnvelope />}
+                        colorScheme="green"
+                        variant="semisolid"
+                        isDisabled={isOpen}
+                    >
+                        Email
+                    </Button>
+                    <EmailInputModal isOpen={isOpen} onClose={onClose} />
 
-                <Button
-                    onClick={() => router.push("/groups/poap")}
-                    leftIcon={<FaAward />}
-                    colorScheme="purple"
-                    variant="semisolid"
-                    isDisabled={isOpen}
-                >
-                    POAP
-                </Button>
-            </HStack>
-        </Container>
+                    <Button
+                        onClick={() => router.push("/groups/poap")}
+                        leftIcon={<FaAward />}
+                        colorScheme="purple"
+                        variant="semisolid"
+                        isDisabled={isOpen}
+                    >
+                        POAP
+                    </Button>
+                </HStack>
+            </Container>
+            <Container flex="1" mb="80px" mt="180px" px="80px" maxW="container.lg">
+                <Heading as="h2" mb="30px" size="xl">
+                    On-chain Groups
+                    <Tooltip
+                        label="Select one of our supported providers to join a on-chain group."
+                        placement="right-start"
+                    >
+                        <span>
+                            <Icon
+                                boxSize="20px"
+                                ml="10px"
+                                mb="5px"
+                                color={colorMode === "light" ? "gray.500" : "background.200"}
+                                as={FaInfoCircle}
+                            />
+                        </span>
+                    </Tooltip>
+                </Heading>
+                <HStack spacing={4} align="left">
+                    <Button
+                        onClick={() => router.push("/onchain/brightid")}
+                        colorScheme="orange"
+                        variant="semisolid"
+                        isDisabled={!!session}
+                    >
+                        BrightId
+                    </Button>
+                </HStack>
+            </Container>
+        </>
     )
 }
 
