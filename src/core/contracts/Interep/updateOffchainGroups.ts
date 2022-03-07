5c9ba422b0be125b1384a572670e527fe034d4f8
cedoor <me@cedoor.dev>
2022-03-07 10:51:15 +0100

    fix: update contract parameters

--- src/core/contracts/Interep/updateOffchainGroups.ts
+++ src/core/contracts/Interep/updateOffchainGroups.ts
@@ -1,6 +1,5 @@
 /* istanbul ignore file */
-import { ContractReceipt } from "ethers"
-import { solidityKeccak256 } from "ethers/lib/utils"
+import { ContractReceipt, utils } from "ethers"
 import config, { ContractName } from "src/config"
 import { GroupName, Provider } from "src/types/groups"
 import { getBackendContractInstance } from "src/utils/backend"
@@ -14,18 +13,18 @@ export default async function updateOffchainGroups(
     const contractAddress = getContractAddress(ContractName.INTEREP)
     const contractInstance = await getBackendContractInstance(ContractName.INTEREP, contractAddress)
 
-    const groupIds = []
     const groups = []
 
     for (let i = 0; i < providers.length; i++) {
-        groupIds.push(solidityKeccak256(["string", "string"], [providers[i], names[i]]))
         groups.push({
+            provider: utils.formatBytes32String(providers[i]),
+            name: utils.formatBytes32String(names[i]),
             depth: config.MERKLE_TREE_DEPTH,
             root: roots[i]
         })
     }
 
-    const transaction = await contractInstance.updateOffchainGroups(groupIds, groups)
+    const transaction = await contractInstance.updateOffchainGroups(groups)
 
     return transaction.wait(1)
 }
