db06ba275414ac369b883def6ce83cc62177bd5f
cedoor <me@cedoor.dev>
2022-03-25 11:00:57 +0100

    refactor: update button primary bg and color

--- src/styles/components/Button.ts
+++ src/styles/components/Button.ts
@@ -1,4 +1,5 @@
 import { SystemStyleObject } from "@chakra-ui/react"
+import { StyleFunctionProps } from "@chakra-ui/theme-tools"
 
 const Button = {
     baseStyle: {
@@ -11,6 +12,30 @@ const Button = {
         size: "lg"
     },
     variants: {
+        solid: (props: StyleFunctionProps): SystemStyleObject => {
+            const { colorScheme: c } = props
+
+            if (c === "primary") {
+                const bg = `${c}.500`
+                const color = "white"
+                const hoverBg = `${c}.600`
+                const activeBg = `${c}.700`
+
+                return {
+                    bg,
+                    color,
+                    _hover: {
+                        bg: hoverBg,
+                        _disabled: {
+                            bg
+                        }
+                    },
+                    _active: activeBg
+                }
+            }
+
+            return {}
+        },
         link: (): SystemStyleObject => ({
             _hover: {
                 textDecoration: "none"
