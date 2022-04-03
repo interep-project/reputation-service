c7f21b0d616bbc2e006937e56f2c79b08dc6e43b
cedoor <me@cedoor.dev>
2022-04-03 23:03:21 +0200

    fix: remove wrong align property

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,8 +4,8 @@ import { FaSadTear } from "react-icons/fa"
 
 export default function NotFoundPage(): JSX.Element {
     return (
-        <Container align="center" flex="1" mb="80px" mt="300px" px="80px" maxW="container.lg">
-            <Heading as="h2" size="xl">
+        <Container flex="1" mb="80px" mt="300px" px="80px" maxW="container.lg">
+            <Heading textAlign="center" as="h2" size="xl">
                 404 Not Found <Icon boxSize="35px" as={FaSadTear} />
             </Heading>
         </Container>
