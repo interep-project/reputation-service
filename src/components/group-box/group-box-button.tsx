1a1141ea45fd68e73a5d52edc84998f8140f1c09
cedoor <me@cedoor.dev>
2022-04-07 13:36:33 +0200

    refactor: move alert in group box button

--- src/components/group-box/group-box-button.tsx
+++ src/components/group-box/group-box-button.tsx
@@ -1,7 +1,19 @@
-import { Button } from "@chakra-ui/react"
-import React from "react"
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogCloseButton,
+    AlertDialogContent,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogOverlay,
+    Button,
+    useDisclosure
+} from "@chakra-ui/react"
+import React, { useRef } from "react"
 
 export type GroupBoxButtonProps = {
+    alertTitle: string
+    alertMessage: string
     children: string
     onClick: () => void
     loading?: boolean
@@ -9,14 +21,19 @@ export type GroupBoxButtonProps = {
 }
 
 export function GroupBoxButton({
+    alertTitle,
+    alertMessage,
     children,
     onClick,
     loading = false,
     disabled = false
 }: GroupBoxButtonProps): JSX.Element {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const cancelRef = useRef() as any
+
     return (
         <Button
-            onClick={onClick}
+            onClick={onOpen}
             colorScheme="background"
             mt="6"
             size="sm"
@@ -25,6 +42,34 @@ export function GroupBoxButton({
             isFullWidth
         >
             {children}
+
+            <AlertDialog
+                motionPreset="slideInBottom"
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+                isOpen={isOpen}
+                isCentered
+            >
+                <AlertDialogOverlay />
+
+                <AlertDialogContent>
+                    <AlertDialogHeader>{alertTitle}</AlertDialogHeader>
+                    <AlertDialogCloseButton ref={cancelRef} />
+                    <AlertDialogBody>{alertMessage}</AlertDialogBody>
+                    <AlertDialogFooter>
+                        <Button
+                            colorScheme="primary"
+                            isFullWidth
+                            onClick={() => {
+                                onClose()
+                                onClick()
+                            }}
+                        >
+                            {children}
+                        </Button>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
         </Button>
     )
 }
