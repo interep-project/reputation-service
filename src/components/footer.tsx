ece2996deac042d95bc3838aaf4c9ed41b8b29d1
cedoor <me@cedoor.dev>
2022-04-08 13:29:11 +0200

    refactor: update footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
-import { FaGithub, FaMedium, FaTelegram, FaTwitter } from "react-icons/fa"
+import { Box, Container, HStack, Icon, Link, Text, useColorMode } from "@chakra-ui/react"
 import React from "react"
-import { Container, Link, Icon, Text, HStack, Box, useColorMode } from "@chakra-ui/react"
+import { FaDiscord, FaGithub } from "react-icons/fa"
 
 export default function Footer(): JSX.Element {
     const { colorMode } = useColorMode()
@@ -9,21 +9,20 @@ export default function Footer(): JSX.Element {
         <Box bg={colorMode === "light" ? "gray.200" : "background.800"} w="100%">
             <Container my="15px" px="80px" maxW="container.xl">
                 <HStack justify="space-between">
-                    <Text>Copyright © 2021 Interep</Text>
-
-                    <HStack spacing={4}>
-                        <Link fontWeight="bold" href="https://docs.interep.link" isExternal>
+                    <HStack spacing="4">
+                        <Text>Copyright © 2021 Interep</Text>
+                        <Text>|</Text>
+                        <Link fontWeight="bold" href="https://interep.link" isExternal>
+                            About
+                        </Link>
+                        <Link fontWeight="bold" href="https://interep.link/docs" isExternal>
                             Docs
                         </Link>
-                        <Text>|</Text>
-                        <Link href="https://medium.com/privacy-scaling-explorations" isExternal>
-                            <Icon boxSize="20px" as={FaMedium} />
-                        </Link>
-                        <Link href="https://twitter.com/PrivacyScaling" isExternal>
-                            <Icon boxSize="20px" as={FaTwitter} />
-                        </Link>
-                        <Link href="https://t.me/interrep" isExternal>
-                            <Icon boxSize="20px" as={FaTelegram} />
+                    </HStack>
+
+                    <HStack spacing={4}>
+                        <Link href="https://discord.gg/Tp9He7qws4" isExternal>
+                            <Icon boxSize="20px" as={FaDiscord} />
                         </Link>
                         <Link href="https://github.com/interep-project" isExternal>
                             <Icon boxSize="20px" as={FaGithub} />
