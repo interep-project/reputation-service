d23d6c997f37035448db490a9d34bc30a4543e6e
cedoor <me@cedoor.dev>
2022-04-08 13:20:28 +0200

    refactor: create custom steps style

--- src/styles/components/index.ts
+++ src/styles/components/index.ts
@@ -1,8 +1,8 @@
-import { StepsStyleConfig as Steps } from "chakra-ui-steps"
 import Button from "./Button"
 import CloseButton from "./CloseButton"
 import Link from "./Link"
 import Modal from "./Modal"
+import Steps from "./Steps"
 import Table from "./Table"
 import Tooltip from "./Tooltip"
 
