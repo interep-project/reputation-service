45cbc4081e340d6b7ec4e594892a5af5db71517f
cedoor <me@cedoor.dev>
2022-04-11 18:36:12 +0200

    refactor: update footer style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,10 @@
-import { Box, Container, HStack, Icon, Link, Text, useColorMode } from "@chakra-ui/react"
+import { Box, Container, HStack, Icon, Link, Text } from "@chakra-ui/react"
 import React from "react"
 import { FaDiscord, FaGithub } from "react-icons/fa"
 
 export default function Footer(): JSX.Element {
-    const { colorMode } = useColorMode()
-
     return (
-        <Box bg={colorMode === "light" ? "gray.200" : "background.800"} w="100%">
+        <Box bg="background.800" w="100%">
             <Container my="15px" px="80px" maxW="container.xl">
                 <HStack justify="space-between">
                     <HStack spacing="4">
@@ -21,11 +19,11 @@ export default function Footer(): JSX.Element {
                     </HStack>
 
                     <HStack spacing={4}>
-                        <Link href="https://discord.gg/Tp9He7qws4" isExternal>
-                            <Icon boxSize="20px" as={FaDiscord} />
+                        <Link href="https://discord.gg/Tp9He7qws4" h={5} isExternal>
+                            <Icon as={FaDiscord} w={5} h={5} />
                         </Link>
-                        <Link href="https://github.com/interep-project" isExternal>
-                            <Icon boxSize="20px" as={FaGithub} />
+                        <Link href="https://github.com/interep-project" h={5} isExternal>
+                            <Icon as={FaGithub} w={5} h={5} />
                         </Link>
                     </HStack>
                 </HStack>
